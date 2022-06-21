5101a74af166a5e6deed1c3d87dd348bc72f1806
VinRicci <vinicioricci@gmail.com>
2022-06-20 22:46:18 -0600

    Tarjetas informativas

--- components/core/Card/Card.tsx
+++ components/core/Card/Card.tsx
@@ -1,4 +1,5 @@
 import styles from './Card.module.scss'
+import prueba from './Card2.module.scss'
 
 type Props = {
   title: string
@@ -13,11 +14,26 @@ const Card = ({ title }: Props) => {
       <div className={styles.flip_card}>
         <div className={styles.flip_card_inner}>
           <div className={styles.flip_card_front}
-            style={{ backgroundImage: `url("https://cdn.sstatic.net/Img/home/illo-public.svg?v=14bd5a506009")` }}
+            style={{ backgroundImage: `url("http://www.odsarte.pr.gov.br/sites/ods-arte/arquivos_restritos/files/imagem/2020-09/img_12.jpg")` }}
           >
           </div>
           <div className={styles.flip_card_back}>
-            Studio Ghibli, dirigida y creada por Hayao Miyazaki en 1988. Tras realizar esta película, se adoptó como logotipo del estudio a los Totoros. Es la tercera película del estudio tras su fundación.
+            {title}
+          </div>
+        </div>
+      </div>
+      <div className={prueba.card}>
+        <div className={prueba.imgbox} style={{ backgroundImage: `url("https://c.tenor.com/YQiF2mfEsYwAAAAC/infinite-infinitely.gif")` }}>
+        </div>
+        <div className={prueba.details}>
+          <div className='title'>
+            <h3>Titulo de prueba<br></br>
+              <small>Contenido de prueba</small>
+            </h3>
+          </div>
+          <div className='description'>
+            <h4>Descripcion</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis, iure eius, adipisci fugit voluptatibus sapiente distinctio assumenda dicta ducimus quo? Iste dolorem quod cumque enim corporis iure veniam rerum.</p>
           </div>
         </div>
       </div>
