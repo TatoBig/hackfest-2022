1cddb022951dcabfb76290aad7b4c7c8e0c2d04e
TatoBig <tatobig@gmail.com>
2022-06-26 00:04:21 -0600

    Added animated hackfest logo

--- pages/credits.tsx
+++ pages/credits.tsx
@@ -1,10 +1,15 @@
+import HackfestLogo from 'components/animation/HackfestLogo'
 import Credits from 'components/core/Credits'
+import styles from './credits.module.scss'
 
 const credits = () => {
   return (
-        <div>
-            <Credits></Credits>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <HackfestLogo />
+        <Credits></Credits>
+      </div>
+    </div>
   )
 }
 
