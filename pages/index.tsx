5e529da9374e3596d43a7ed43f0b64d090557622
PaoloVeliz <paoloveliz55@gmail.com>
2022-06-26 16:11:48 -0600

    Footer added to the layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Intro from 'components/animation/Intro'
 import HomeAbout from 'components/core/HomeAbout'
 import ObjectiveContent from 'components/core/ObjectiveContent'
 import ObjectiveContentRight from 'components/core/ObjectiveContentRight'
-import FooterClass from 'scomponents/core/FooterClass'
+import FooterClass from 'components/core/FooterClass'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from './index.module.scss'
@@ -55,7 +55,6 @@ const Home: NextPage = () => {
         los sistemas tributarios y la eliminación gradual de los subsidios perjudiciales, cuando existan, para reflejar su 
         impacto ambiental, teniendo plenamente en cuenta las necesidades y condiciones específicas de los países en desarrollo
          y minimizando los posibles efectos adversos en su desarrollo, de manera que se proteja a los pobres y a las comunidades afectadas." image="https://unsplash.it/400" />
-      <FooterClass />
     </div>
   )
 }
