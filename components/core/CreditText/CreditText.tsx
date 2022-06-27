901fdb9ab08dddb7b22e0acefc6009efa8abb77e
VinRicci <vinicioricci@gmail.com>
2022-06-26 18:00:26 -0600

    Credit box hecha

--- components/core/CreditText/CreditText.tsx
+++ components/core/CreditText/CreditText.tsx
@@ -1,26 +1,28 @@
 import { useState, useEffect } from 'react'
 import styles from './CreditText.module.scss'
 
-type Props = {delay?: number}
+type Props = {
+  delay?: number
+}
 
-const CreditText = ({ delay = 0 }: Props) => {
+const CreditText = ({ delay = 4 }: Props) => {
   const [initial, setinitial] = useState(false)
-  const [initial2, setinitial2] = useState(false)
+
   useEffect(() => {
     texto()
   }, [])
+
   const texto = () => {
     setTimeout(() => {
       setinitial(true)
     }, delay * 1000)
-    setTimeout(() => {
-      setinitial2(true)
-    }, (delay * 1000) + 1000)
   }
+
   return (
-    <div className={styles.container}>
-        <div className={initial ? styles.final : styles.inicio}></div>
-        <div className={initial2 ? styles.final_texto : styles.inicio_texto} style={{ transitionDelay: '1s' }}>Integrantes holaadios</div>
+    <div className={initial ? styles.final : styles.inicio}>
+      <div style={{ transitionDelay: '0.5s' }} className={initial ? styles.final_texto : styles.inicio_texto}>
+        Integrantes
+      </div>
     </div>
   )
 }
