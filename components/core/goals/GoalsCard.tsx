02a0432c8f636ebe6dbb01df94b1b9f9f29ff85a
Alevsdl <alejandravsdl@gmail.com>
2022-06-26 21:40:44 -0600

    Hipervinculos agregados

--- components/core/goals/GoalsCard.tsx
+++ components/core/goals/GoalsCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from './GoalsCard.module.scss'
 
 type Props = {
@@ -8,7 +9,7 @@ const GoalsCard = ({ l }: Props) => {
   return (
   <div className={styles.container}>
     <div className={styles.content}>
-        <h3>{l}</h3>
+      <Link href={l}>Mas información...</Link>
     </div>
     <div className={styles.flap}></div>
   </div>
