1bd95ed8189d549b3a21dd392c92408ece1f9bc9
PaoloVeliz <paoloveliz55@gmail.com>
2022-07-01 08:41:01 -0600

    Footer text changed

--- components/core/FooterClass/FooterClass.tsx
+++ components/core/FooterClass/FooterClass.tsx
@@ -4,7 +4,8 @@ import styles from './FooterClass.module.scss'
 const FooterClass = () => {
   return (
     <div className={styles.Footer}>
-          @Copyright LOS INDUSTRIALES 2022- None of any posible Right Reserved.
+          @Copyright LOS JEFFREYS 2022- None of any posible Right Reserved. Estudiantes de Ingenieria informatica y sistemas  de la Universidad Rafael Landivar, Campus San Alberto Hurtado, S.J Quetzaltenango.
+
     </div>
   )
 }
